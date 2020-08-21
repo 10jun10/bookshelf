71faaa6dda595d8132a54a2d11547e77f2cdb269
jun.katami <tami708j_airuy@yahoo.co.jp>
2020-08-21 18:19:53 +0900

    for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'staticpages/top'
+  root 'staticpages#top'
   get 'staticpages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
